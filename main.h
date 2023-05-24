4becbbc7560f542c336c94d025108ad250396c00
Nizar-Bahajoub <n.bahajoub@gmail.com>
2023-05-24 18:44:24 +0100

    Updated

--- main.h
+++ main.h
@@ -88,11 +88,11 @@ int executeCommandCommand(char *command, int lineNum);
 int executeCommandWithPath(char **args);
 ssize_t getLine(char **buffer, size_t *bufsize, int fd);
 int changeDirectory(char **args, int lineNum);
-int echoCommand(char **args);
+int echoCommand(char **args, int lineNum)
 int runChildProcess(char **args);
 int runParentProcess(void);
 int getExitStatus(void);
-
+int echoCommand(char **args, int lineNum)
 char *stringTokenize(char *command, const char *delim);
 int tokenizeCommand(char *command, char **args);
 int isBuiltInCommand(char **args, int lineNum);
